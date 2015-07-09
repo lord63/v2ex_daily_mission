b86c3bbd8ea8305d42f60f3986ca11ffe52c5139
lord63 <lord63.j@gmail.com>
2015-07-09 21:21:16 +0800

    Add tests for `v2ex last`

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from os import path
+from codecs import open
 
 import pytest
 import responses
@@ -16,25 +17,25 @@ ROOT = path.join(path.dirname(path.abspath(__file__)), 'responses')
 @pytest.yield_fixture
 def mock_api():
 
-    with open(path.join(ROOT, 'signin.html')) as f:
+    with open(path.join(ROOT, 'signin.html'), encoding='utf-8') as f:
         mock_signin_body = f.read()
     responses.add(responses.POST, 'https://www.v2ex.com/signin',
                   body=mock_signin_body)
     responses.add(responses.GET, 'https://www.v2ex.com/signin',
                   body=mock_signin_body)
 
-    with open(path.join(ROOT, 'once.html')) as f:
-        mock_signin_body = f.read()
+    with open(path.join(ROOT, 'once.html'), encoding='utf-8') as f:
+        mock_once_body = f.read()
     responses.add(responses.GET,
                   'https://www.v2ex.com/mission/daily/redeem?once=51947',
-                  body=mock_signin_body)
+                  body=mock_once_body)
 
-    with open(path.join(ROOT, 'balance.html')) as f:
+    with open(path.join(ROOT, 'balance.html'), encoding='utf-8') as f:
         mock_balance_body = f.read()
     responses.add(responses.GET, 'https://www.v2ex.com/balance',
                   body=mock_balance_body)
 
-    with open(path.join(ROOT, 'mission.html')) as f:
+    with open(path.join(ROOT, 'mission.html'), encoding='utf-8') as f:
         mock_mission_body = f.read()
     responses.add(responses.GET, 'https://www.v2ex.com/mission/daily',
                   body=mock_mission_body)
